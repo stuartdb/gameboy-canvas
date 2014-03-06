0141458eb576042c76adf7c544570c6a03d7cbb8
Stuart Baker <sdb@stuartdb.com>
2014-03-06 14:56:07 +1300

    Rename the variables used for console dimensions

--- gb.js
+++ gb.js
@@ -2,19 +2,19 @@ function draw_gb() {
     'use strict';
     var canvas,
         gb,
-        w,
-        h,
-        x,
-        y,
+        gb_w,
+        gb_h,
+        gb_x,
+        gb_y,
         gb_sc,
         gb_bc;
 
     // width and height of console
-    w = 430;
-    h = 700;
+    gb_w = 430;
+    gb_h = 700;
     // x and y start position
-    x = 0;
-    y = 0;
+    gb_x = 0;
+    gb_y = 0;
     // curve sizes for the console corners
     gb_sc = 20;
     gb_bc = 100;
@@ -24,23 +24,21 @@ function draw_gb() {
 
     // draw the outline of the console
     gb.beginPath();
-
     // start with top left edge curve
-    gb.moveTo(x, gb_sc);
-    gb.quadraticCurveTo(x, y, gb_sc, y);
-    gb.lineTo(w-gb_sc, 0);
-    gb.quadraticCurveTo(w, y, w, gb_sc);
-    gb.lineTo(w, h-gb_bc);
-    gb.quadraticCurveTo(w, h, w-gb_bc, h);
-    gb.lineTo(gb_sc, h);
-    gb.quadraticCurveTo(x, h, x, h-gb_sc);
-    gb.lineTo(x, y+gb_sc);
-
+    gb.moveTo(gb_x, gb_sc);
+    gb.quadraticCurveTo(gb_x, gb_y, gb_sc, gb_y);
+    gb.lineTo(gb_w - gb_sc, gb_y);
+    gb.quadraticCurveTo(gb_w, gb_y, gb_w, gb_sc);
+    gb.lineTo(gb_w, gb_h - gb_bc);
+    gb.quadraticCurveTo(gb_w, gb_h, gb_w - gb_bc, gb_h);
+    gb.lineTo(gb_sc, gb_h);
+    gb.quadraticCurveTo(gb_x, gb_h, gb_x, gb_h - gb_sc);
+    gb.lineTo(gb_x, gb_y + gb_sc);
+    gb.closePath();
     // fill the console
     gb.fillStyle = 'rgb(190,186,183)';
     gb.fill();
-
-    // describe the 'pen'
+    // describe the 'pen' and draw the line
     gb.lineWidth = 2;
     gb.strokeStyle = 'rgb(190,186,183)';
     gb.stroke();
