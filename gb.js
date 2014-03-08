919bbfbdd9f994dca6fd47e753a5885f4da9c944
Stuart Baker <sdb@stuartdb.com>
2014-03-08 21:07:37 +1300

    Large refactor. Variable name changes and changed to passing co ords to functions instead of relative locations.

--- gb.js
+++ gb.js
@@ -1,23 +1,23 @@
 /**
 * Rotate the context by the specified angle
-* @param {Object} con Canvas context to rotate
+* @param {Object} context Canvas context to rotate
 * @param {Number} x X position to rotate around
 * @param {Number} y Y position to rotate around
 * @param {Number} r Angle in radians
 **/
-function rotate_context(con, x, y, r) {
+function rotate_context(context, x, y, r) {
     'use strict';
     // translate context to x,y, rotate it, return to 'real' 0,0
-    con.translate(x, y);
-    con.rotate(r);
-    con.translate(-x, -y);
+    context.translate(x, y);
+    context.rotate(r);
+    context.translate(-x, -y);
 }
 
 /**
 * Draws a rectangle in the shape of the gameboy console.
 * It's fairly unique in that the lower right corner has a large curve.
 * All other corners have a small curve.
-* @param {Object} con Canvas context to draw rect on.
+* @param {Object} context Canvas context to draw rect on.
 * @param {String} color RGB color string 'rgb(0,0,0)'
 * @param {Number} x X position to start drawing top left corner of rect
 * @param {Number} y Y position to start drawing top left corner of rect
@@ -26,21 +26,21 @@ function rotate_context(con, x, y, r) {
 * @param {Number} sc Size of the small corners
 * @param {Number} bc Size of the lower right large corner
 **/
-function draw_gb_rect(con, color, x, y, w, h, sc, bc) {
+function draw_gb_rect(context, color, x, y, w, h, sc, bc) {
     'use strict';
-    con.beginPath();
-    con.moveTo(x, y + sc);
-    con.quadraticCurveTo(x, y, x + sc, y);
-    con.lineTo(x + w - sc, y);
-    con.quadraticCurveTo(x + w, y, x + w, y + sc);
-    con.lineTo(x + w, y + h - bc);
-    con.quadraticCurveTo(x + w, y + h, x + w - bc, y + h);
-    con.lineTo(x + sc, y + h);
-    con.quadraticCurveTo(x, y + h, x, y + h - sc);
-    con.lineTo(x, y + sc);
-    con.closePath();
-    con.fillStyle = color;
-    con.fill();
+    context.beginPath();
+    context.moveTo(x, y + sc);
+    context.quadraticCurveTo(x, y, x + sc, y);
+    context.lineTo(x + w - sc, y);
+    context.quadraticCurveTo(x + w, y, x + w, y + sc);
+    context.lineTo(x + w, y + h - bc);
+    context.quadraticCurveTo(x + w, y + h, x + w - bc, y + h);
+    context.lineTo(x + sc, y + h);
+    context.quadraticCurveTo(x, y + h, x, y + h - sc);
+    context.lineTo(x, y + sc);
+    context.closePath();
+    context.fillStyle = color;
+    context.fill();
 }
 
 /**
@@ -48,7 +48,7 @@ function draw_gb_rect(con, color, x, y, w, h, sc, bc) {
 * The height represents the size of the sides that will have curved ends
 * Which means the width will almost always be the longest dimension.
 * If you want the opposite to that, specify an angle of 90
-* @param {Object} con Canvas context to draw rect on.
+* @param {Object} context Canvas context to draw rect on.
 * @param {String} color RGB color string 'rgb(0,0,0)'
 * @param {Number} cx X position of the centre of the rect
 * @param {Number} cy Y position of the centre of the rect
@@ -56,7 +56,7 @@ function draw_gb_rect(con, color, x, y, w, h, sc, bc) {
 * @param {Number} h Height of the rect
 * @param {Number} a The angle of the rect
 **/
-function draw_curved_rect(con, color, cx, cy, w, h, a) {
+function draw_curved_rect(context, color, cx, cy, w, h, a) {
     'use strict';
     // x,y is set to top left of rect after curve to begin pathing
     // r is angle converted to radians which is what rotate accepts
@@ -64,22 +64,22 @@ function draw_curved_rect(con, color, cx, cy, w, h, a) {
         y = cy - h * 0.5,
         r = (Math.PI / 180) * a;
 
-    rotate_context(con, cx, cy, r);
-    con.beginPath();
-    con.moveTo(x, y);
-    con.lineTo(x + w - h, y);
-    con.bezierCurveTo(x + w, y, x + w, y + h, x + w - h, y + h);
-    con.lineTo(x, y + h);
-    con.bezierCurveTo(x - h, y + h, x - h, y, x, y);
-    con.closePath();
-    con.fillStyle = color;
-    con.fill();
-    rotate_context(con, cx, cy, -r);
+    rotate_context(context, cx, cy, r);
+    context.beginPath();
+    context.moveTo(x, y);
+    context.lineTo(x + w - h, y);
+    context.bezierCurveTo(x + w, y, x + w, y + h, x + w - h, y + h);
+    context.lineTo(x, y + h);
+    context.bezierCurveTo(x - h, y + h, x - h, y, x, y);
+    context.closePath();
+    context.fillStyle = color;
+    context.fill();
+    rotate_context(context, cx, cy, -r);
 }
 
 /**
 * Draws a rectangle with smoothed corners.
-* @param {Object} con Canvas context to draw rect on.
+* @param {Object} context Canvas context to draw rect on.
 * @param {String} color RGB color string 'rgb(0,0,0)'
 * @param {Number} cx X position of the centre of the rect
 * @param {Number} cy Y position of the centre of the rect
@@ -88,7 +88,7 @@ function draw_curved_rect(con, color, cx, cy, w, h, a) {
 * @param {Number} cs Curve size, size of rounded edges
 * @param {Number} a Angle of the rect
 **/
-function draw_smooth_rect(con, color, cx, cy, w, h, sc, a) {
+function draw_smooth_rect(context, color, cx, cy, w, h, sc, a) {
     'use strict';
     // x,y is set to top left of rect (0,0)
     // r is angle converted to radians which is what rotate accepts
@@ -96,43 +96,43 @@ function draw_smooth_rect(con, color, cx, cy, w, h, sc, a) {
         y = cy - h * 0.5,
         r = (Math.PI / 180) * a;
 
-    rotate_context(con, cx, cy, r);
-    con.beginPath();
-    con.moveTo(x, y + sc);
-    con.quadraticCurveTo(x, y, x + sc, y);
-    con.lineTo(x + w - sc, y);
-    con.quadraticCurveTo(x + w, y, x + w, y + sc);
-    con.lineTo(x + w, y + h - sc);
-    con.quadraticCurveTo(x + w, y + h, x + w - sc, y + h);
-    con.lineTo(x + sc, y + h);
-    con.quadraticCurveTo(x, y + h, x, y + h - sc);
-    con.lineTo(x, y + sc);
-    con.closePath();
-    con.fillStyle = color;
-    con.fill();
-    rotate_context(con, cx, cy, -r);
+    rotate_context(context, cx, cy, r);
+    context.beginPath();
+    context.moveTo(x, y + sc);
+    context.quadraticCurveTo(x, y, x + sc, y);
+    context.lineTo(x + w - sc, y);
+    context.quadraticCurveTo(x + w, y, x + w, y + sc);
+    context.lineTo(x + w, y + h - sc);
+    context.quadraticCurveTo(x + w, y + h, x + w - sc, y + h);
+    context.lineTo(x + sc, y + h);
+    context.quadraticCurveTo(x, y + h, x, y + h - sc);
+    context.lineTo(x, y + sc);
+    context.closePath();
+    context.fillStyle = color;
+    context.fill();
+    rotate_context(context, cx, cy, -r);
 }
 
 /**
 * Draws circle at specified location
-* @param {Object} con Canvas context to draw rect on.
+* @param {Object} context Canvas context to draw rect on.
 * @param {String} color RGB color string 'rgb(0,0,0)'
 * @param {Number} x X position of the centre of the circle
 * @param {Number} y Y position of the centre of the circle
 * @param {Number} r Radius of circle
 **/
-function draw_circle(con, color, x, y, r) {
+function draw_circle(context, color, x, y, r) {
     'use strict';
-    con.beginPath();
-    con.arc(x, y, r, 0, Math.PI * 2);
-    con.closePath();
-    con.fillStyle = color;
-    con.fill();
+    context.beginPath();
+    context.arc(x, y, r, 0, Math.PI * 2);
+    context.closePath();
+    context.fillStyle = color;
+    context.fill();
 }
 
 /**
 * Draws a line between two locations
-* @param {Object} con Canvas context to draw line on.
+* @param {Object} context Canvas context to draw line on.
 * @param {String} color RGB color string 'rgb(0,0,0)'
 * @param {Number} sx X position of the start of the line
 * @param {Number} sy Y position of the start of the line
@@ -140,164 +140,127 @@ function draw_circle(con, color, x, y, r) {
 * @param {Number} ey Y position of the end of the line
 * @param {Number} w Width or thickness of the line
 **/
-function draw_line(con, color, sx, sy, ex, ey, w) {
+function draw_line(context, color, sx, sy, ex, ey, w) {
     "use strict";
-    con.beginPath();
-    con.moveTo(sx, sy);
-    con.lineTo(ex, ey);
-    con.closePath();
-    con.lineWidth = w;
-    con.strokeStyle = color;
-    con.stroke();
+    context.beginPath();
+    context.moveTo(sx, sy);
+    context.lineTo(ex, ey);
+    context.closePath();
+    context.lineWidth = w;
+    context.strokeStyle = color;
+    context.stroke();
 }
 
 /**
 * Draws a triangle at the specified location
-* @param {Object} con Canvas context to draw trigangle on.
+* @param {Object} context Canvas context to draw trigangle on.
 * @param {String} color RGB color string 'rgb(0,0,0)'
 * @param {Number} cx X position of the center of the triangle
 * @param {Number} cy Y position of the center of the triangle
 * @param {Number} w Width or length of one side
 * @param {Number} a Angle of the triangle
 **/
-function draw_triangle(con, color, cx, cy, w, a) {
+function draw_triangle(context, color, cx, cy, w, a) {
     "use strict";
     var x = cx - w / 2,
         y = cy + w / 2,
         r = (Math.PI / 180) * a;
 
-    rotate_context(con, cx, cy, r);
-    con.beginPath();
-    con.moveTo(x, y);
-    con.lineTo(x + w, y);
-    con.lineTo(x + w / 2, y - w);
-    con.lineTo(x, y);
-    con.closePath();
-    con.fillStyle = color;
-    con.fill();
-    rotate_context(con, cx, cy, -r);
+    rotate_context(context, cx, cy, r);
+    context.beginPath();
+    context.moveTo(x, y);
+    context.lineTo(x + w, y);
+    context.lineTo(x + w / 2, y - w);
+    context.lineTo(x, y);
+    context.closePath();
+    context.fillStyle = color;
+    context.fill();
+    rotate_context(context, cx, cy, -r);
 }
 
-
+/**
+* Main function to draw the Gameboy to the canvas.
+*
+* It may seem like a tedious approach to manually enter co ords for everything.
+* Relying on relative positions to other shapes gets super messy though,
+* especially when you feel like making very small adjustments to random parts.
+**/
 function draw_gb() {
     'use strict';
     var canvas,
-        gb,
-        gb_x = 0,
-        gb_y = 0,
-        gb_w = 360,
-        gb_h = 592,
-        gb_sc = 10,
-        gb_bc = 70,
-        gb_sc_bg_x = 30,
-        gb_sc_bg_y = 50,
-        gb_sc_bg_h = 230,
-        gb_sc_bg_w = gb_w - gb_sc_bg_x * 2,
-        gb_sc_bg_sc = 10,
-        gb_sc_bg_bc = 40,
-        gb_sc_x = 85,
-        gb_sc_y = 80,
-        gb_sc_h = 170,
-        gb_sc_w = gb_w - gb_sc_x * 2,
-        gb_dpad_x = 75,
-        gb_dpad_y = 400,
-        gb_dpad_w = 45,
-        gb_dpad_h = 30,
-        gb_bbut_rad = 20,
-        gb_bbut_x = 250,
-        gb_bbut_y = gb_dpad_y,
-        gb_abut_rad = gb_bbut_rad,
-        gb_abut_x = gb_sc_bg_x + gb_sc_bg_w - gb_abut_rad,
-        gb_abut_y = gb_bbut_y - gb_bbut_rad,
-        c_ab = 'rgb(140,30,80)',
-        c_black = 'rgb(0,0,0)',
-        c_shell = 'rgb(190,190,190)',
-        c_face = 'rgb(88,88,100)',
-//        c_grill = 'rgb(200,200,200)',
-        c_grill = 'rgb(0,0,0)',
-        c_battery = 'rgb(40,40,40)',
-        c_blue = 'rgb(20,30,120)',
-        c_screen = 'rgb(80,100,20)';
+        context,
+        color_a_b = 'rgb(140,30,80)',
+        color_dpad = 'rgb(0,0,0)',
+        color_shell = 'rgb(190,190,190)',
+        color_face = 'rgb(88,88,100)',
+        color_detail = 'rgb(200,200,200)',
+        color_battery = 'rgb(40,40,40)',
+        color_line_1 = 'rgb(140,30,80)',
+        color_line_2 = 'rgb(20,30,120)',
+        color_screen = 'rgb(80,100,20)';
 
-    canvas = document.getElementById('gb');
-    gb = canvas.getContext('2d');
+    canvas = document.getElementById('gameboy');
+    context = canvas.getContext('2d');
 
-    draw_gb_rect(gb, c_shell, gb_x, gb_y, gb_w, gb_h, gb_sc, gb_bc);
-    draw_gb_rect(gb,
-                 c_face,
-                 gb_sc_bg_x,
-                 gb_sc_bg_y,
-                 gb_sc_bg_w,
-                 gb_sc_bg_h,
-                 gb_sc_bg_sc,
-                 gb_sc_bg_bc);
+    // console
+    draw_gb_rect(context, color_shell, 0, 0, 360, 592, 10, 70);
 
-    // draw the screen
-    gb.fillStyle = c_screen;
-    gb.fillRect(gb_sc_x, gb_sc_y, gb_sc_w, gb_sc_h);
+    // screen background
+    draw_gb_rect(context, color_face, 30, 50, 300, 230, 10, 40);
 
-    draw_smooth_rect(gb,
-                     c_black,
-                     gb_dpad_x,
-                     gb_dpad_y,
-                     gb_dpad_w * 2,
-                     gb_dpad_h,
-                     6,
-                     0);
+    // screen
+    context.fillStyle = color_screen;
+    context.fillRect(85, 80, 190, 170);
 
-    draw_smooth_rect(gb,
-                     c_black,
-                     gb_dpad_x,
-                     gb_dpad_y,
-                     gb_dpad_w * 2,
-                     gb_dpad_h,
-                     6,
-                     90);
+    // dpad
+    draw_smooth_rect(context, color_dpad, 75, 400, 90, 30, 6, 0);
+    draw_smooth_rect(context, color_dpad, 75, 400, 90, 30, 6, 90);
 
-    draw_circle(gb, c_ab, gb_bbut_x, gb_bbut_y, gb_bbut_rad);
-    draw_circle(gb, c_ab, gb_abut_x, gb_abut_y, gb_abut_rad);
+    // a and b buttons
+    draw_circle(context, color_a_b, 250, 400, 20);
+    draw_circle(context, color_a_b, 310, 380, 20);
 
     // start and select
-    draw_curved_rect(gb, c_face, 130, 490, 40, 15, 337.5);
-    draw_curved_rect(gb, c_face, 190, 490, 40, 15, 337.5);
+    draw_curved_rect(context, color_face, 130, 490, 40, 15, 337.5);
+    draw_curved_rect(context, color_face, 190, 490, 40, 15, 337.5);
 
     // grill
-    draw_curved_rect(gb, c_grill, 255, 555, 50, 5, 67.5);
-    draw_curved_rect(gb, c_grill, 270, 547.5, 50, 5, 67.5);
-    draw_curved_rect(gb, c_grill, 285, 540, 50, 5, 67.5);
-    draw_curved_rect(gb, c_grill, 300, 532.5, 50, 5, 67.5);
-    draw_curved_rect(gb, c_grill, 315, 525, 50, 5, 67.5);
-    draw_curved_rect(gb, c_grill, 330, 517.5, 50, 5, 67.5);
+    draw_curved_rect(context, color_detail, 255, 555, 50, 5, 67.5);
+    draw_curved_rect(context, color_detail, 270, 547.5, 50, 5, 67.5);
+    draw_curved_rect(context, color_detail, 285, 540, 50, 5, 67.5);
+    draw_curved_rect(context, color_detail, 300, 532.5, 50, 5, 67.5);
+    draw_curved_rect(context, color_detail, 315, 525, 50, 5, 67.5);
+    draw_curved_rect(context, color_detail, 330, 517.5, 50, 5, 67.5);
 
     // battery light
-    draw_circle(gb, c_battery, 55, 140, 5);
+    draw_circle(context, color_battery, 55, 140, 5);
 
     // headphone detail
-    draw_curved_rect(gb, c_grill, 160, 575, 40, 15, 0);
-    draw_line(gb, c_grill, 165, 575, 165, 592, 3);
-    draw_line(gb, c_grill, 172, 575, 172, 592, 3);
-    draw_line(gb, c_grill, 179, 575, 179, 592, 3);
+    draw_curved_rect(context, color_detail, 160, 575, 40, 15, 0);
+    draw_line(context, color_detail, 165, 575, 165, 592, 3);
+    draw_line(context, color_detail, 172, 575, 172, 592, 3);
+    draw_line(context, color_detail, 179, 575, 179, 592, 3);
 
     // on off detail
-    draw_curved_rect(gb, c_grill, 80, 15, 50, 15, 0);
-    draw_line(gb, c_grill, 70, 15, 70, 0, 3);
-    draw_line(gb, c_grill, 77, 15, 77, 0, 3);
-    draw_line(gb, c_grill, 84, 15, 84, 0, 3);
+    draw_curved_rect(context, color_detail, 80, 15, 50, 15, 0);
+    draw_line(context, color_detail, 70, 15, 70, 0, 3);
+    draw_line(context, color_detail, 77, 15, 77, 0, 3);
+    draw_line(context, color_detail, 84, 15, 84, 0, 3);
 
     // lines at top of console
-    draw_line(gb, c_grill, 0, 30, 360, 30, 3);
-    draw_line(gb, c_grill, 30, 0, 30, 30, 3);
-    draw_line(gb, c_grill, 330, 0, 330, 30, 3);
+    draw_line(context, color_detail, 0, 30, 360, 30, 3);
+    draw_line(context, color_detail, 30, 0, 30, 30, 3);
+    draw_line(context, color_detail, 330, 0, 330, 30, 3);
 
-    // lines within the screen background
-    draw_line(gb, c_ab, 40, 60, 320, 60, 3);
-    draw_line(gb, c_blue, 40, 65, 320, 65, 3);
+    // lines on the screen face
+    draw_line(context, color_line_1, 40, 60, 320, 60, 3);
+    draw_line(context, color_line_2, 40, 65, 320, 65, 3);
 
-    // draw small triangle details around dpad
-    draw_triangle(gb, c_grill, 75, 350, 7, 0);
-    draw_triangle(gb, c_grill, 25, 400, 7, 270);
-    draw_triangle(gb, c_grill, 125, 400, 7, 90);
-    draw_triangle(gb, c_grill, 75, 450, 7, 180);
+    // small triangle details around dpad
+    draw_triangle(context, color_detail, 75, 350, 7, 0);
+    draw_triangle(context, color_detail, 25, 400, 7, 270);
+    draw_triangle(context, color_detail, 125, 400, 7, 90);
+    draw_triangle(context, color_detail, 75, 450, 7, 180);
 }
 
 draw_gb();
